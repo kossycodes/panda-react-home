f40e0a2640eefd9cc3737e8cf8db5b0e058f2d94
kossycodes <kosisochukwuezeagu042@gmail.com>
2023-04-11 11:43:58 +0100

    first

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/kossycodes/panda-react-home",
+  base: "panda-react-home",
   plugins: [react({
     include: "**/*.jsx",
   })],
